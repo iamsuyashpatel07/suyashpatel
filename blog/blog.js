98aff52b956f1e1fa733b67f7216da9f758f4282
[suyash patel] <suyashpatelmrj@gmail.com>
2021-04-07 16:10:54 +0530

    updated portfolio section ploblem wth image shrink

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,9 @@
-var i = 0
-
 function Count() {
-    document.getElementById("OUTPUT").innerHTML = "LIKE " + (i++ + 1);
+    let i = 0;
+    document.getElementById("OUTPUT").innerHTML = "LIKE " + (++i);
+}
+
+function Count2() {
+    let k = 0;
+    document.getElementById("OUT").innerHTML = "LIKE " + (++k);
 }
